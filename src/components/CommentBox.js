f9e3314ecc8e32e822d40e4e4dfdea79fc4c4bcd
Natthaphon Phanit <nulladivides@protonmail.com>
2018-06-23 15:25:55 +0700

    finished

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { comment: '' };
-  // }
     state = { comment: '' };
 
     handleChange = (event) => {
@@ -13,20 +11,25 @@ class CommentBox extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
+        this.props.saveComment(this.state.comment);
         this.setState({ comment: '' });
     };
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <h4>Add a Comment</h4>
-                <textarea onChange={this.handleChange} value={this.state.comment} />
-                <div>
-                    <button>Submit Comment</button>
-                </div>
-            </form>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <h4>Add a Comment</h4>
+                    <textarea onChange={this.handleChange} value={this.state.comment} />
+                    <div>
+                        <button>Submit Comment</button>
+                    </div>
+                </form>
+                <button className="fetch-comments" onClick={this.props.fetchComments}>Fetch Comments</button>
+            </div>
+            
         );
     }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-    return (
-        <div>
-        Comment List
-        </div>
-    );
+class CommentList extends Component {
+    state = {  }
+    renderComments() {
+        return this.props.comments.map(comment => (
+            <li key={comment}>{comment}</li>
+        ));
+    }
+    render() {
+        return (
+            <ul>
+                {this.renderComments()}
+            </ul>
+        );
+    }
 }
+
+function mapStateToProps({ comments }) {
+    return { comments };
+}
+
+export default connect(mapStateToProps)(CommentList);
